592563f5f8dd0edf453152d0f3488617d9ab363d
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2014-01-06 15:25:23 -0800

    Delete duplicate migration files

--- db/migrate/20131119170940_add_title_to_job_column.rb
+++
@@ -1,5 +0,0 @@
-class AddTitleToJobColumn < ActiveRecord::Migration
-  def change
-    add_column :job_columns, :title, :string
-  end
-end
--- db/migrate/20131119190552_create_job_column_contents.rb
+++
@@ -1,9 +0,0 @@
-class CreateJobColumnContents < ActiveRecord::Migration
-  def change
-    create_table :job_column_contents do |t|
-      t.string :content
-
-      t.timestamps
-    end
-  end
-end
