efc82b6bc85e2b860b0b742322a0695e07640dbe
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2014-03-10 12:01:50 -0700

    Rollback database

--- db/schema.rb
+++ db/schema.rb
@@ -11,24 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140309234757) do
+ActiveRecord::Schema.define(version: 20140227230108) do
 
   create_table "job_groups", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
     t.integer  "user_id"
-  end
-
-  create_table "job_uploads", force: true do |t|
-    t.integer  "job_id"
-    t.string   "upload_file_name"
-    t.string   "upload_content_type"
-    t.integer  "upload_file_size"
-    t.datetime "upload_updated_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "url"
   end
 
   create_table "jobs", force: true do |t|
